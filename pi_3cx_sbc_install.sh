3544bc6419e04c479489e462693175a162ad6380
Dan Edwards <dan.jj.edwards@gmail.com>
2021-03-26 16:05:05 +0000

    Update pi_3cx_sbc_install.sh
    Added pi password change with option for default or unique password

--- pi_3cx_sbc_install.sh
+++ pi_3cx_sbc_install.sh
@@ -6,6 +6,7 @@ function ask_yes_or_no() {
         *)     echo "no" ;;
     esac
 }
+PASS = e4syTr1d3nt
 echo "#####IBT Rpi 3CX SBC install script#####"
 echo "Please, enter hostname to use for device monitoring - e.g gch.svg.sbc.3cx.co.uk"
 read NAME
@@ -21,7 +22,18 @@ then
       exit 0
       fi
 fi
-echo "Great, continuing to update packages and install monitoring..."
+if [[ "no" == $(ask_yes_or_no "Set password to IBT default?")]]
+    then
+        echo "Please enter password."
+        read PASS
+        if [[ "no" == $(ask_yes_or_no "Password will be set to $PASS, is that correct?") || \
+          "no" == $(ask_yes_or_no "Are you *really* sure?") ]]
+          then
+            echo "Please re-run install script with correct details"
+            exit 0
+            fi
+fi
+           echo "Great, continuing to update packages and install monitoring..."
 echo "Checking for updates..."
 if ! /usr/bin/sudo /usr/bin/apt update 2>&1 | grep -q '^[WE]:'; then
     echo "Update check completed" 
@@ -35,6 +47,7 @@ fi
 #  echo "$OUTPUT"
 #fi
 #/usr/bin/sudo /usr/bin/apt update
+sudo sh -c 'echo pi:$PASS | chpasswd'
 echo "Upgrading as needed..."
 /usr/bin/sudo /usr/bin/apt -y upgrade
 echo "Installing monitoring agent..."
@@ -44,7 +57,7 @@ echo "Configuring monitoring agent..."
 # edit zabbix_agentd.conf replace server=127.0.0.1 with server=213.218.197.155 set hostname to $NAME
 sed -i s/^Server=127.0.0.1/Server=213.218.197.155/ /etc/zabbix/zabbix_agentd.conf
 sed -i s/^ServerActive=127.0.0.1/ServerActive=213.218.197.155 /etc/zabbix/zabbix_agentd.conf
-sed -i s/^Hostname=/Hostname=$NAME/ /etc/zabbix/zabbix_agentd.conf
+sed -i s/^\# Hostname=/Hostname=$NAME/ /etc/zabbix/zabbix_agentd.conf
 /usr/bin/curl -o /etc/zabbix/zabbix_agentd.conf.d/userparameter_rpi.conf https://raw.githubusercontent.com/danjeman/rpi-zabbix/main/userparameter_rpi.conf
 /usr/sbin/usermod -a -G video zabbix
 /usr/bin/sudo /usr/sbin/service zabbix-agent restart
